025a599372a5a2fbcd255c25cd5da136a3b1afe7
Kylie Clark <kyliedarlene@icloud.com>
2024-02-03 18:37:21 -0700

    complete lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci = [0, 1]
+    if length > 2:
+        for i in range(length - 2):
+            fibonacci.append(fibonacci[-1] + fibonacci[-2])
+    elif length == 1:
+        fibonacci.pop()
+    elif length == 0:
+        fibonacci.clear()
+    print(fibonacci)
